a9d3922447b5c9d2ed607dfc50ed1736f1f67861
ed.xyz <eddietang2314@gmail.com>
2025-07-13 08:14:02 -0400

    asdfasd

--- utils/tkinter_compat.py
+++ utils/tkinter_compat.py
@@ -6,7 +6,7 @@ def set_window_transparency(window, color="black", alpha=0.8):
     if plat.startswith("win"):
         window.wm_attributes("-transparentcolor", color)
     elif plat == "darwin":
-        # window.wm_attributes("-alpha", alpha)
+        window.wm_attributes("-alpha", 0)
         window.wm_attributes("-transparent", True)
     else:
         window.wm_attributes("-alpha", alpha)
