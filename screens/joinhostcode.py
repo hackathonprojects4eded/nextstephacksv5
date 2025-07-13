7a6691b0478ccfb7616148ba610351752f53574b
ed.xyz <eddietang2314@gmail.com>
2025-07-13 08:44:48 -0400

    asdf

--- screens/joinhostcode.py
+++ screens/joinhostcode.py
@@ -23,7 +23,10 @@ class JoinHostCodeScreen:
         self.root.geometry(geometry_str)
         self.root.configure(bg=self.transparent_color)
         set_window_transparency(self.root, color=self.transparent_color, alpha=0.8)
-        self.root.overrideredirect(True)
+        import sys
+
+        if sys.platform != "darwin":
+            self.root.overrideredirect(True)
         self.root.wm_attributes("-topmost", True)
 
         self._drag_start_pointer_x = 0
